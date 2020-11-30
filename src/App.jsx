74114d96e4bd8084aed92bf2c7227055fb8fa3ab
Federico Soncini <federicosoncini@Federicos-Air.fritz.box>
2020-11-30 16:45:30 +0100

    delete app imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
-import ModalForm from "./components/ModalForm"
 
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
-      <ModalForm />
     </div>
   )
 }
