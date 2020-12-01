800ac94e80edb842a5bb4de096915aa7fdf305e4
massiliatazit <massiliatazit@users.noreply.github.com>
2020-12-01 01:00:33 +0100

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-import NavBar from "./components/NavBar"
+import NavBar from "./components/NavBar";
 
-import React from "react"
-import Profile from "./components/Profile"
+import React from "react";
+import Profile from "./components/Profile";
+import Footer from "./components/Footer";
 
 class App extends React.Component {
   state = {
     me: true,
-  }
+  };
   render() {
     return (
       <div className="App">
         <NavBar />
         <Profile me={this.state.me} />
+        <Footer />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
