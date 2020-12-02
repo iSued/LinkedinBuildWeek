2bcee9ae709dce0b321566d58b3668af8e0ef920
iSued <59610863+iSued@users.noreply.github.com>
2020-12-02 23:22:51 +0100

    done

--- src/components/Know.jsx
+++ src/components/Know.jsx
@@ -3,8 +3,13 @@ import { Row, Col, Container, Accordion, Card, Button } from "react-bootstrap";
 import { Link, withRouter } from "react-router-dom";
 import { Avatar } from "@material-ui/core";
 import MessageSharpIcon from "@material-ui/icons/MessageSharp";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 
-class YouKnow extends React.Component {
+class Know extends React.Component {
+  state = {
+    users1: this.props.someProps,
+    users2: this.props.otherProps,
+  };
   render() {
     return (
       <div className="mb-2 youknow">
@@ -14,105 +19,12 @@ class YouKnow extends React.Component {
               <Container>
                 <h3>you know</h3>
               </Container>
-              <Row>
-                <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
-                  <Avatar
-                    alt="Remy Sharp"
-                    src="https://picsum.photos/id/237/200/300"
-                    className="avatar"
-                  />
-                </Col>
-                <Col
-                  xs={7}
-                  className="bordered d-flex px-2 py-2 justify-content-start align-items-center"
-                >
-                  <Link>
-                    <h4>John Hello</h4>
-                    <span>Burger seller at facebook</span>
-                  </Link>
-                </Col>
-                <Col className="bordered d-flex px-2 py-2 justify-content-center align-items-center">
-                  <MessageSharpIcon />
-                </Col>
-              </Row>
-
-              <Row>
-                <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
-                  <Avatar
-                    alt="Remy Sharp"
-                    src="https://picsum.photos/id/237/200/300"
-                    className="avatar"
-                  />
-                </Col>
-                <Col
-                  xs={7}
-                  className=" bordered d-flex px-2 py-2 justify-content-start align-items-center"
-                >
-                  <Link>
-                    <h4>John Hello</h4>
-                    <span>Burger seller at facebook</span>
-                  </Link>
-                </Col>
-                <Col className="bordered d-flex px-2 py-2 justify-content-center align-items-center">
-                  <MessageSharpIcon />
-                </Col>
-              </Row>
-              <Row>
-                <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
-                  <Avatar
-                    alt="Remy Sharp"
-                    src="https://picsum.photos/id/237/200/300"
-                    className="avatar"
-                  />
-                </Col>
-                <Col
-                  xs={7}
-                  className=" bordered d-flex px-2 py-2 justify-content-start align-items-center"
-                >
-                  <Link>
-                    <h4>John Hello</h4>
-                    <span>Burger seller at facebook</span>
-                  </Link>
-                </Col>
-                <Col className="bordered d-flex px-2 py-2 justify-content-center align-items-center">
-                  <MessageSharpIcon />
-                </Col>
-              </Row>
-              <Row>
-                <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
-                  <Avatar
-                    alt="Remy Sharp"
-                    src="https://picsum.photos/id/237/200/300"
-                    className="avatar"
-                  />
-                </Col>
-                <Col
-                  xs={7}
-                  className=" d-flex px-2 py-2 justify-content-start align-items-center"
-                >
-                  <Link>
-                    <h4>John Hello</h4>
-                    <span>Burger seller at facebook</span>
-                  </Link>
-                </Col>
-                <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
-                  <MessageSharpIcon />
-                </Col>
-              </Row>
-            </Card.Body>
-            <Card.Header className=" d-flex px-2 py-2 justify-content-center align-items-center">
-              <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                Click me!
-              </Accordion.Toggle>
-            </Card.Header>
-
-            <Accordion.Collapse eventKey="0">
-              <Card.Body>
+              {this.props.oneProps.map((user) => (
                 <Row>
                   <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
                     <Avatar
                       alt="Remy Sharp"
-                      src="https://picsum.photos/id/237/200/300"
+                      src={user.image}
                       className="avatar"
                     />
                   </Col>
@@ -120,85 +32,57 @@ class YouKnow extends React.Component {
                     xs={7}
                     className="bordered d-flex px-2 py-2 justify-content-start align-items-center"
                   >
-                    <Link>
-                      <h4>John Hello</h4>
-                      <span>Burger seller at facebook</span>
+                    <Link to={"/profile/" + user._id}>
+                      <h4>
+                        {user.name} {user.surname}
+                      </h4>
+                      <p>{user.title}</p>
                     </Link>
                   </Col>
                   <Col className="bordered d-flex px-2 py-2 justify-content-center align-items-center">
                     <MessageSharpIcon />
                   </Col>
                 </Row>
-
-                <Row>
-                  <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
-                    <Avatar
-                      alt="Remy Sharp"
-                      src="https://picsum.photos/id/237/200/300"
-                      className="avatar"
-                    />
-                  </Col>
-                  <Col
-                    xs={7}
-                    className=" bordered d-flex px-2 py-2 justify-content-start align-items-center"
-                  >
-                    <Link>
-                      <h4>John Hello</h4>
-                      <span>Burger seller at facebook</span>
-                    </Link>
-                  </Col>
-                  <Col className="bordered d-flex px-2 py-2 justify-content-center align-items-center">
-                    <MessageSharpIcon />
-                  </Col>
-                </Row>
-                <Row>
-                  <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
-                    <Avatar
-                      alt="Remy Sharp"
-                      src="https://picsum.photos/id/237/200/300"
-                      className="avatar"
-                    />
-                  </Col>
-                  <Col
-                    xs={7}
-                    className=" bordered d-flex px-2 py-2 justify-content-start align-items-center"
-                  >
-                    <Link>
-                      <h4>John Hello</h4>
-                      <span>Burger seller at facebook</span>
-                    </Link>
-                  </Col>
-                  <Col className="bordered d-flex px-2 py-2 justify-content-center align-items-center">
-                    <MessageSharpIcon />
-                  </Col>
-                </Row>
-                <Row>
-                  <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
-                    <Avatar
-                      alt="Remy Sharp"
-                      src="https://picsum.photos/id/237/200/300"
-                      className="avatar"
-                    />
-                  </Col>
-                  <Col
-                    xs={7}
-                    className=" d-flex px-2 py-2 justify-content-start align-items-center"
-                  >
-                    <Link>
-                      <h4>John Hello</h4>
-                      <span>Burger seller at facebook</span>
-                    </Link>
-                  </Col>
-                  <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
-                    <MessageSharpIcon />
-                  </Col>
-                </Row>
+              ))}
+            </Card.Body>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>
+                {this.props.twoProps.map((user) => (
+                  <Row>
+                    <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
+                      <Avatar
+                        alt="Remy Sharp"
+                        src={user.image}
+                        className="avatar"
+                      />
+                    </Col>
+                    <Col
+                      xs={7}
+                      className="bordered d-flex px-2 py-2 justify-content-start align-items-center"
+                    >
+                      <Link to={"/profile/" + user._id}>
+                        <h4>
+                          {user.name} {user.surname}
+                        </h4>
+                        <p>{user.title}</p>
+                      </Link>
+                    </Col>
+                    <Col className="bordered d-flex px-2 py-2 justify-content-center align-items-center">
+                      <MessageSharpIcon />
+                    </Col>
+                  </Row>
+                ))}
               </Card.Body>
             </Accordion.Collapse>
+            <Card.Header className=" d-flex px-2 py-2 justify-content-center align-items-center">
+              <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                Show more <ArrowDropDownIcon />
+              </Accordion.Toggle>
+            </Card.Header>
           </Card>
         </Accordion>
       </div>
     );
   }
 }
-export default withRouter(YouKnow);
+export default withRouter(Know);
--- src/components/MayKnow.jsx
+++ src/components/MayKnow.jsx
@@ -3,6 +3,7 @@ import { Row, Col, Container, Accordion, Card, Button } from "react-bootstrap";
 import { Link, withRouter } from "react-router-dom";
 import { Avatar } from "@material-ui/core";
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 
 class MayKnow extends React.Component {
   render() {
@@ -14,105 +15,12 @@ class MayKnow extends React.Component {
               <Container>
                 <h3>People you may know</h3>
               </Container>
-              <Row>
-                <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
-                  <Avatar
-                    alt="Remy Sharp"
-                    src="https://picsum.photos/id/237/200/300"
-                    className="avatar"
-                  />
-                </Col>
-                <Col
-                  xs={7}
-                  className="bordered d-flex px-2 py-2 justify-content-start align-items-center"
-                >
-                  <Link>
-                    <h4>John Hello</h4>
-                    <span>Burger seller at facebook</span>
-                  </Link>
-                </Col>
-                <Col className="bordered d-flex px-2 py-2 justify-content-center align-items-center">
-                  <AddCircleOutlineIcon />
-                </Col>
-              </Row>
-
-              <Row>
-                <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
-                  <Avatar
-                    alt="Remy Sharp"
-                    src="https://picsum.photos/id/237/200/300"
-                    className="avatar"
-                  />
-                </Col>
-                <Col
-                  xs={7}
-                  className=" bordered d-flex px-2 py-2 justify-content-start align-items-center"
-                >
-                  <Link>
-                    <h4>John Hello</h4>
-                    <span>Burger seller at facebook</span>
-                  </Link>
-                </Col>
-                <Col className="bordered d-flex px-2 py-2 justify-content-center align-items-center">
-                  <AddCircleOutlineIcon />
-                </Col>
-              </Row>
-              <Row>
-                <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
-                  <Avatar
-                    alt="Remy Sharp"
-                    src="https://picsum.photos/id/237/200/300"
-                    className="avatar"
-                  />
-                </Col>
-                <Col
-                  xs={7}
-                  className=" bordered d-flex px-2 py-2 justify-content-start align-items-center"
-                >
-                  <Link>
-                    <h4>John Hello</h4>
-                    <span>Burger seller at facebook</span>
-                  </Link>
-                </Col>
-                <Col className="bordered d-flex px-2 py-2 justify-content-center align-items-center">
-                  <AddCircleOutlineIcon />
-                </Col>
-              </Row>
-              <Row>
-                <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
-                  <Avatar
-                    alt="Remy Sharp"
-                    src="https://picsum.photos/id/237/200/300"
-                    className="avatar"
-                  />
-                </Col>
-                <Col
-                  xs={7}
-                  className=" d-flex px-2 py-2 justify-content-start align-items-center"
-                >
-                  <Link>
-                    <h4>John Hello</h4>
-                    <span>Burger seller at facebook</span>
-                  </Link>
-                </Col>
-                <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
-                  <AddCircleOutlineIcon />
-                </Col>
-              </Row>
-            </Card.Body>
-            <Card.Header className=" d-flex px-2 py-2 justify-content-center align-items-center">
-              <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                Click me!
-              </Accordion.Toggle>
-            </Card.Header>
-
-            <Accordion.Collapse eventKey="0">
-              <Card.Body>
+              {this.props.threeProps.map((user) => (
                 <Row>
                   <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
                     <Avatar
                       alt="Remy Sharp"
-                      src="https://picsum.photos/id/237/200/300"
+                      src={user.image}
                       className="avatar"
                     />
                   </Col>
@@ -120,81 +28,54 @@ class MayKnow extends React.Component {
                     xs={7}
                     className="bordered d-flex px-2 py-2 justify-content-start align-items-center"
                   >
-                    <Link>
-                      <h4>John Hello</h4>
-                      <span>Burger seller at facebook</span>
+                    <Link to={"/profile/" + user._id}>
+                      <h4>
+                        {user.name} {user.surname}
+                      </h4>
+                      <p>{user.title}</p>
                     </Link>
                   </Col>
                   <Col className="bordered d-flex px-2 py-2 justify-content-center align-items-center">
                     <AddCircleOutlineIcon />
                   </Col>
                 </Row>
+              ))}
+            </Card.Body>
 
-                <Row>
-                  <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
-                    <Avatar
-                      alt="Remy Sharp"
-                      src="https://picsum.photos/id/237/200/300"
-                      className="avatar"
-                    />
-                  </Col>
-                  <Col
-                    xs={7}
-                    className=" bordered d-flex px-2 py-2 justify-content-start align-items-center"
-                  >
-                    <Link>
-                      <h4>John Hello</h4>
-                      <span>Burger seller at facebook</span>
-                    </Link>
-                  </Col>
-                  <Col className="bordered d-flex px-2 py-2 justify-content-center align-items-center">
-                    <AddCircleOutlineIcon />
-                  </Col>
-                </Row>
-                <Row>
-                  <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
-                    <Avatar
-                      alt="Remy Sharp"
-                      src="https://picsum.photos/id/237/200/300"
-                      className="avatar"
-                    />
-                  </Col>
-                  <Col
-                    xs={7}
-                    className=" bordered d-flex px-2 py-2 justify-content-start align-items-center"
-                  >
-                    <Link>
-                      <h4>John Hello</h4>
-                      <span>Burger seller at facebook</span>
-                    </Link>
-                  </Col>
-                  <Col className="bordered d-flex px-2 py-2 justify-content-center align-items-center">
-                    <AddCircleOutlineIcon />
-                  </Col>
-                </Row>
-                <Row>
-                  <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
-                    <Avatar
-                      alt="Remy Sharp"
-                      src="https://picsum.photos/id/237/200/300"
-                      className="avatar"
-                    />
-                  </Col>
-                  <Col
-                    xs={7}
-                    className=" d-flex px-2 py-2 justify-content-start align-items-center"
-                  >
-                    <Link>
-                      <h4>John Hello</h4>
-                      <span>Burger seller at facebook</span>
-                    </Link>
-                  </Col>
-                  <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
-                    <AddCircleOutlineIcon />
-                  </Col>
-                </Row>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>
+                {this.props.fourProps.map((user) => (
+                  <Row>
+                    <Col className=" d-flex px-2 py-2 justify-content-center align-items-center">
+                      <Avatar
+                        alt="Remy Sharp"
+                        src={user.image}
+                        className="avatar"
+                      />
+                    </Col>
+                    <Col
+                      xs={7}
+                      className="bordered d-flex px-2 py-2 justify-content-start align-items-center"
+                    >
+                      <Link to={"/profile/" + user._id}>
+                        <h4>
+                          {user.name} {user.surname}
+                        </h4>
+                        <p>{user.title}</p>
+                      </Link>
+                    </Col>
+                    <Col className="bordered d-flex px-2 py-2 justify-content-center align-items-center">
+                      <AddCircleOutlineIcon />
+                    </Col>
+                  </Row>
+                ))}
               </Card.Body>
             </Accordion.Collapse>
+            <Card.Header className=" d-flex px-2 py-2 justify-content-center align-items-center">
+              <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                Show more <ArrowDropDownIcon />
+              </Accordion.Toggle>
+            </Card.Header>
           </Card>
         </Accordion>
       </div>
