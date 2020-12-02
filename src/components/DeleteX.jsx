634792e41253e1051855620dd271595541328218
Ermal-code <72197798+Ermal-code@users.noreply.github.com>
2020-12-02 21:31:21 +0100

    style for deleteX

--- src/components/DeleteX.jsx
+++ src/components/DeleteX.jsx
@@ -1,11 +1,13 @@
-import React from "react"
+import React from "react";
 
 export default function DeleteX(props) {
   return (
     <i
-      className={props.me === true ? " fas fa-times  ml-3 mr-1 p-2" : "d-none"}
-      style={{color: props.color}}
+      className={
+        props.me === true ? " fas fa-times  ml-3 mr-1 py-1 px-2" : "d-none"
+      }
+      style={{ color: props.color }}
       onClick={() => props.onClicked()}
     ></i>
-  )
+  );
 }
